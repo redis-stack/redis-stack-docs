7224258fce84e4a881496edbe522c3c6a2ad336e
slorello89 <slorello89@gmail.com>
2022-05-18 15:57:40 -0400

    Enabling Netlify previews

---
+++ netlify.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm -rf redis-stack-website
+git clone --recurse-submodules https://$PRIVATE_ACCESS_TOKEN@github.com/redis-stack/redis-stack-website
+cd redis-stack-website
+npm install autoprefixer
+REDIS_STACK_DOCS_SHA=$COMMIT_REF REDIS_STACK_DOCS_REMOTE=$REPOSITORY_URL make netlify
