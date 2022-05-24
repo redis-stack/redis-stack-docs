c0529a324690b6ee2f95b18e2c2d99429e6bf2bd
slorello89 <slorello89@gmail.com>
2022-05-24 11:07:22 -0400

    removing pointers to slorello89

--- netlify.bash
+++ netlify.bash
@@ -2,9 +2,8 @@
 
 rm -rf redis-stack-website
 repo_dir=$(pwd)
-git clone --recurse-submodules https://$PRIVATE_ACCESS_TOKEN@github.com/slorello89/redis-stack-website
+git clone --recurse-submodules https://$PRIVATE_ACCESS_TOKEN@github.com/redis-stack/redis-stack-website
 cd redis-stack-website
 pip3 install -r requirements.txt
-git checkout netlify-previews
 npm install autoprefixer
 REDIS_STACK_DOCS_DIR=$repo_dir make netlify
