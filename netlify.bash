bbb502ea4a9cb4b9b09fcff95aa0964b2d537e5d
slorello89 <slorello89@gmail.com>
2022-05-24 10:51:27 -0400

    removing pipfile trying to build from requirements

--- netlify.bash
+++ netlify.bash
@@ -4,6 +4,7 @@ rm -rf redis-stack-website
 cwd=$(pwd)
 git clone --recurse-submodules https://$PRIVATE_ACCESS_TOKEN@github.com/slorello89/redis-stack-website
 cd redis-stack-website
+pip3 install -r requirements.txt
 git checkout netlify-previews
 npm install autoprefixer
 REDIS_STACK_DOCS_DIR=$cwd make netlify
