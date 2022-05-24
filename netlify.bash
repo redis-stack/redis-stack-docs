90c49b6b545491195ab0e45a64685c7406b0995d
slorello89 <slorello89@gmail.com>
2022-05-24 10:48:21 -0400

    from dir rather than git

--- netlify.bash
+++ netlify.bash
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 rm -rf redis-stack-website
-git clone --recurse-submodules https://$PRIVATE_ACCESS_TOKEN@github.com/redis-stack/redis-stack-website
+cwd=$(pwd)
+git clone --recurse-submodules https://$PRIVATE_ACCESS_TOKEN@github.com/slorello89/redis-stack-website
 cd redis-stack-website
-
-printenv
-
+git checkout netlify-previews
 npm install autoprefixer
-REDIS_STACK_DOCS_SHA=$COMMIT_REF REDIS_STACK_DOCS_REMOTE=$REPOSITORY_URL make netlify
+REDIS_STACK_DOCS_DIR=$cwd make netlify
