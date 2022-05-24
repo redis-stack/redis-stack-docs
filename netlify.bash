f6bf82517d911eee61de51717943818f52111f61
slorello89 <slorello89@gmail.com>
2022-05-24 10:05:31 -0400

    spooky

--- netlify.bash
+++ netlify.bash
@@ -4,8 +4,7 @@ rm -rf redis-stack-website
 git clone --recurse-submodules https://$PRIVATE_ACCESS_TOKEN@github.com/redis-stack/redis-stack-website
 cd redis-stack-website
 
-echo $REPOSITORY_URL
-echo $COMMIT_REF
+printenv
 
 npm install autoprefixer
 REDIS_STACK_DOCS_SHA=$COMMIT_REF REDIS_STACK_DOCS_REMOTE=$REPOSITORY_URL make netlify
