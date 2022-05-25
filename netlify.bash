b0f5fbf5a989ea7b2d56531b7c73eb67c65596e4
Itamar Haber <itamar@redis.com>
2022-05-25 18:42:04 +0300

    Adds paramaterizable target path

--- netlify.bash
+++ netlify.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Prepares the Netlify preview
 : ${REDIS_STACK_REPOSITORY:="github.com/redis-stack/redis-stack-website"}
+: ${TARGET_PATH:="/tmp/stack"}
 if [[ -n $PRIVATE_ACCESS_TOKEN ]]; then
     REDIS_STACK_REPOSITORY="$PRIVATE_ACCESS_TOKEN@$REDIS_STACK_REPOSITORY"
 fi
 repo_dir=$(pwd)
 
-cd /tmp
-rm -rf stack
-git clone --recurse-submodules -o stack https://$REDIS_STACK_REPOSITORY
+rm -rf $TARGET_PATH
+git clone --recurse-submodules https://$REDIS_STACK_REPOSITORY $TARGET_PATH
+cd $TARGET_PATH
 REPO_DIR=$repo_dir REPOSITORY_URL=$REPOSITORY_URL PREVIEW_MODE=1 make deps netlify
