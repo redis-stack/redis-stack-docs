15d7df966201bb80e507a4d0e4dacf8b524f81f1
Itamar Haber <itamar@redis.com>
2022-05-25 18:54:18 +0300

    Slightly refactors preview script (#52)

--- netlify.bash
+++ netlify.bash
@@ -1,8 +1,12 @@
 #!/bin/bash
-rm -rf redis-stack-website
+# Prepares the Netlify preview
+: ${REDIS_STACK_REPOSITORY:="github.com/redis-stack/redis-stack-website"}
+if [[ -n $PRIVATE_ACCESS_TOKEN ]]; then
+    REDIS_STACK_REPOSITORY="$PRIVATE_ACCESS_TOKEN@$REDIS_STACK_REPOSITORY"
+fi
 repo_dir=$(pwd)
-git clone --recurse-submodules https://$PRIVATE_ACCESS_TOKEN@github.com/redis-stack/redis-stack-website
-cd redis-stack-website
-pip3 install -r requirements.txt
-npm install autoprefixer
-REPO_DIR=$repo_dir REPOSITORY_URL=$REPOSITORY_URL PREVIEW_MODE=1 make netlify
+
+rm -rf website
+git clone --recurse-submodules https://$REDIS_STACK_REPOSITORY website
+cd website
+REPO_DIR=$repo_dir REPOSITORY_URL=$REPOSITORY_URL PREVIEW_MODE=1 make deps netlify
